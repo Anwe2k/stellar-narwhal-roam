91f8c8a531486c2530ea01a974e77f752318f54f
[dyad] <sashaa1755@gmail.com>
2026-06-14 11:48:27 +0200

    [dyad] Wrapped routes in BrowserRouter and Context Providers inside App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 "use client";
 
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from '@/pages/Index';
 import Profile from '@/pages/Profile';
 import ProfileSettings from '@/pages/ProfileSettings';
 import BodyMeasurementsPage from '@/pages/overview/BodyMeasurementsPage';
 import UnitSettingsPage from '@/pages/UnitSettingsPage';
 import Overview from '@/pages/Overview';
+import { HealthDataProvider } from '@/context/HealthDataContext';
+import { UnitProvider } from '@/context/UnitContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/overview" element={<Overview />} />
-      <Route path="/overview/measurements" element={<BodyMeasurementsPage />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/profile-settings" element={<ProfileSettings />} />
-      <Route path="/settings/units" element={<UnitSettingsPage />} />
-    </Routes>
+    <BrowserRouter>
+      <UnitProvider>
+        <HealthDataProvider>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/overview" element={<Overview />} />
+            <Route path="/overview/measurements" element={<BodyMeasurementsPage />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile-settings" element={<ProfileSettings />} />
+            <Route path="/settings/units" element={<UnitSettingsPage />} />
+          </Routes>
+        </HealthDataProvider>
+      </UnitProvider>
+    </BrowserRouter>
   );
 }
 
