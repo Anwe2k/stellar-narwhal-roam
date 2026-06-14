7ddfedfa370d28efdce6cfafcff66ee901314429
[dyad] <sashaa1755@gmail.com>
2026-06-14 11:48:13 +0200

    [dyad] Fixed import errors in App.tsx by correcting routes to existing Overview and UnitSettingsPage components - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ import Index from '@/pages/Index';
 import Profile from '@/pages/Profile';
 import ProfileSettings from '@/pages/ProfileSettings';
 import BodyMeasurementsPage from '@/pages/overview/BodyMeasurementsPage';
-import UnitsSettingsPage from '@/pages/settings/UnitsSettingsPage';
-import OverviewPage from '@/pages/OverviewPage';
+import UnitSettingsPage from '@/pages/UnitSettingsPage';
+import Overview from '@/pages/Overview';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="/overview" element={<OverviewPage />} />
+      <Route path="/overview" element={<Overview />} />
       <Route path="/overview/measurements" element={<BodyMeasurementsPage />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/profile-settings" element={<ProfileSettings />} />
-      <Route path="/settings/units" element={<UnitsSettingsPage />} />
+      <Route path="/settings/units" element={<UnitSettingsPage />} />
     </Routes>
   );
 }
